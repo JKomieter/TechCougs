9cdf0afbbfc3af142b7c5fe72fa49eaa0f1f7c1e
Joel Komieter <komieterj@gmail.com>
2024-01-31 15:42:28 -0500

    try again

--- techcougs/app/competitive_programming/start_challenge/page.tsx
+++ techcougs/app/competitive_programming/start_challenge/page.tsx
@@ -1,14 +1,13 @@
+"use client"
 import Header from '@/layouts/CompProg/Header';
 import React from 'react';
 import Questions from '@/layouts/CompProg/Questions';
 
 function Page() {
-  // Check if window is defined before using it
-  const isClient = typeof window !== 'undefined';
 
   return (
     <main className='w-full min-h-screen'>
-      {isClient && <Header />}
+      <Header />
       <Questions />
     </main>
   );
